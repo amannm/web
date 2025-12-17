e82bd8bfd2038096327b9e7efe665bd9a06345e9
Amann Malik <amannmalik@gmail.com>
2025-12-17 17:42:19 -0600

    agent

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,4 @@ module web.main {
     requires transitive info.picocli;
     requires transitive jakarta.json;
     requires transitive java.net.http;
-    exports com.amannmalik.web.cli;
-    exports com.amannmalik.web.chromium;
-    exports com.amannmalik.web.perception;
-    opens com.amannmalik.web.cli to info.picocli;
 }
